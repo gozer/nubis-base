769703a7f741e1a3e7253d05e2d539273b3df0ba
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-02-02 15:45:07 -0500

    Sentinel to run only inside Atlas/Packer

--- nubis/packer/generated.sh
+++ nubis/packer/generated.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-set -x
+# Detect if we are running packer inside Atlas
+if [ ! -z "$ATLAS_BUILD_ID" ]; then
+  set -x
+  which  librarian-puppet 2>/dev/null 1>/dev/null || sudo apt-get install -y ruby-dev
+  which  librarian-puppet 2>/dev/null 1>/dev/null || ( sudo gem install librarian-puppet -v 2.2.1 && sudo gem install puppet )
+  test -r nubis/Puppetfile && ( cd nubis && librarian-puppet install --path librarian-puppet/etc/puppet/modules --verbose )
+  test -d nubis/librarian-puppet/etc/puppet/modules && tar -C nubis/librarian-puppet --exclude='.git' -zpcf nubis/librarian-puppet.tar.gz .
 
-# Probe Packer build environment
-env
-facter
-dpkg -l
-
-echo "XXX: MARK"
-
-which  librarian-puppet 2>/dev/null 1>/dev/null || sudo apt-get install -y ruby-dev
-which  librarian-puppet 2>/dev/null 1>/dev/null || ( sudo gem install librarian-puppet -v 2.2.1 && sudo gem install puppet )
-test -r nubis/Puppetfile && ( cd nubis && librarian-puppet install --path librarian-puppet/etc/puppet/modules --verbose )
-test -d nubis/librarian-puppet/etc/puppet/modules && tar -C nubis/librarian-puppet --exclude='.git' -zpcf nubis/librarian-puppet.tar.gz .
+  facter
+fi
