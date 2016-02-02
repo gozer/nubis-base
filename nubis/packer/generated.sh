ea6c35030fb3c8fbc57a468f0c1dcadc0274f0f3
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-02-02 15:30:47 -0500

    tyop----

--- nubis/packer/generated.sh
+++ nubis/packer/generated.sh
@@ -3,6 +3,6 @@
 set -x
 
 which  librarian-puppet 2>/dev/null 1>/dev/null || sudo apt-get install -y ruby-dev
-which  librarian-puppet 2>/dev/null 1>/dev/null || ( sudo gem install librarian-puppet -v 2.2.1 && sudo gem install puppet
+which  librarian-puppet 2>/dev/null 1>/dev/null || ( sudo gem install librarian-puppet -v 2.2.1 && sudo gem install puppet )
 test -r nubis/Puppetfile && ( cd nubis && librarian-puppet install --path librarian-puppet/etc/puppet/modules --verbose )
 test -d nubis/librarian-puppet/etc/puppet/modules && tar -C nubis/librarian-puppet --exclude='.git' -zpcf nubis/librarian-puppet.tar.gz .
