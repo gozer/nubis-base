ebd06290cb1fada14dccbfb12a98d757f9eaf6cd
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-06 10:43:52 -0500

    try and install packer

--- run.sh
+++ run.sh
@@ -7,3 +7,12 @@ pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 
 aws s3 ls
+
+PACKER_V=$(packer --version 2>/dev/null)
+
+if [ "$PACKER_V" != "0.12.1" ]; then
+  wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/0.12.1/packer_0.12.1_linux_amd64.zip
+  cd $HOME/bin && unzip /tmp/packer.zip
+fi
+
+packer --version
